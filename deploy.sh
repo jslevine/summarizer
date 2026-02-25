4a5232adb41ba2ea96b26bf1ecb16b38b71cc377
Joshua Levine <jslevine@gmail.com>
2026-02-25 22:08:58 +0900

    Modified /s to default to meetings/ directory and updated the model to gemini-2.0-flash. Updated the system instructions to improve output and keep url links. Also, so the instructions can be removed from the email.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 #
-gcloud functions deploy pdf-summarizer --gen2 --runtime=python311 --region=us-central1 --source=. --entry-point=main_router --trigger-http --allow-unauthenticated --memory=4Gi --timeout=540s --max-instances=200 --cpu=2 --concurrency=2
-
+gcloud functions deploy pdf-summarizer --gen2 --runtime=python311 --region=us-central1 --source=. --entry-point=main_router --trigger-http --allow-unauthenticated --memory=4Gi --timeout=540s --max-instances=200 --cpu=4 --concurrency=8
